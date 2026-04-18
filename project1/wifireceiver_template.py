96debd526707634302f2597c7033c14c5ab9e08e
Ice-Skates <n8snyderr@gmail.com>
2026-04-18 15:40:43 -0700

    level 3

--- project1/wifireceiver_template.py
+++ project1/wifireceiver_template.py
@@ -125,8 +125,12 @@ def WifiReceiver(input_stream, level):
         input_stream=input_stream
 
     if level >= 3:
-        #Input QAM modulated + Encoded Bits + OFDM Symbols
-        #Output QAM modulated + Encoded Bits
+
+        nsym = int(len(input_stream)/nfft)      
+        for i in range(nsym):
+            symbol = input_stream[i*nfft:(i+1)*nfft]
+            output[i*nfft:(i+1)*nfft] = np.fft.fft(symbol)
+
         input_stream=input_stream
     
     if level >= 2:
@@ -178,8 +182,8 @@ from wifitransmitter import WifiTransmitter
 if __name__ == "__main__":
     # test_case = 'The Internet has transformed our everyday lives, bringing people closer together and powering multi-billion dollar industries. The mobile revolution has brought Internet connectivity to the last-mile, connecting billions of users worldwide. But how does the Internet work? What do oft repeated acronyms like "LTE", "TCP", "WWW" or a "HTTP" actually mean and how do they work? This course introduces fundamental concepts of computer networks that form the building blocks of the Internet. We trace the journey of messages sent over the Internet from bits in a computer or phone to packets and eventually signals over the air or wires. We describe commonalities and differences between traditional wired computer networks from wireless and mobile networks. Finally, we build up to exciting new trends in computer networks such as the Internet of Things, 5-G and software defined networking. Topics include: physical layer and coding (CDMA, OFDM, etc.); data link protocol; flow control, congestion control, routing; local area networks (Ethernet, Wi-Fi, etc.); transport layer; and introduction to cellular (LTE) and 5-G networks. The course will be graded based on quizzes (on canvas), a midterm and final exam and four projects (all individual). '
     test_case = 'hello!'
-    output = WifiTransmitter(test_case, 2)
-    begin_zero_padding, message, length_y = WifiReceiver(output, 2)
+    output = WifiTransmitter(test_case, 3)
+    begin_zero_padding, message, length_y = WifiReceiver(output, 3)
     print(test_case)
     print(begin_zero_padding, message, length_y)
     print(test_case == message)
