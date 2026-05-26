c4a4f785469a544b50d776f3304fba9ed20b995a
Geeoon Chung <geeoon13@gmail.com>
2026-05-25 21:35:15 -0700

    Partial work

--- project3/tcpserver.py
+++ project3/tcpserver.py
@@ -2,6 +2,7 @@ import socket, sys
 import json
 import time
 import threading
+import struct
 
 BUFSIZE = 10202  # size of receiving buffer
 PKTSIZE = 10200  # number of bytes in a packet
@@ -33,39 +34,96 @@ class Server():
     
     def find_file(self, file_name):
         #A function to find the peer with the file you want!
-        pass
+        for peer in self.peer_info:
+            if file_name in peer['content_info']
+                return (peer['hostname'], peer['port'])
     
     def load_file(self, file_name):
         # find which server has the file
+        server = find_file(file_name)
+        if not server:
+            raise Exception("Could not find the server which has the requested file.")
         # establish a client socket for downloading file
         self.cl_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-        
+        self.cl_socket.settimeout(TIMEOUT)
+        # connect socket to server
+        self.cl_socket.connect(server)
         # use a connect flag to determine if the file name is sent correctly
-        
+        connect_flag = False
+
         #Initiate three-way handshake and use a connect flag
-        
+        our_sequence_number = 0
+
         while not connect_flag:
             try:
                 # handshake
-                pass
+                # send SYN
+                self.cl_socket.send(b'S' + file_name.encode('ascii'))
+                # receive SYNACK
+                received = self.cl_socket.recv(BUFSIZE)
+                if received != b'B':
+                    # we should receive a '2' in response
+                    raise Exception(f"Did not receive proper SYNACK: {received}")
+                # send ACK
+                self.cl_socket.send(b'A' + struct.pack(">I", our_sequence_number))
+                connect_flag = True
             except socket.timeout:
                 # handshake failed
+                # timeout waiting for SYNACK
+                # resend the SYN, so do nothing
                 pass
       
-        # the receiver keeps a record for which part has been acked
-
         # start receiving file
+        data = b''
+        while connect_flag:
+            try:
+                chunk = self.cl_socket.recv(BUFSIZE)
+                # check the header
+                if chunk[0] == ord('F'):  # check if FIN packet
+                    our_sequence_number += 1
+                    connect_flag = False
+                    continue
+                else chunk[0] != ord('D'):
+                    continue  # we got junk, toss it
+                chunk = chunk[1:]  # strip the header
 
+                seq_num = struct.unpack(">I", chunk[:2])
+                if seq_num == our_sequence_number:
+                    # valid packet
+                    # store and increment our sequence numbers
+                    data += chunk[2:]
+                    our_sequence_number += 1
+                else:
+                    # repeated or out of order packet
+                    pass  # drop
+            except socket.timeout:
+                # socket timed out, meaning we did not receive a packet in time, resend an ACK
+                pass
+            finally:
+                # send the ACK
+                self.cl_socket.send(b'A' + struct.pack(">I", our_sequence_number))
+                
         # transmission complete, close socket
+        self.cl_socket.close()
 
         # write the file
+        with open(file_name, "wb") as file:
+            file.write(data)
         
     def read_file(self, file_name):
         #You can write a function that takes the file to be transmitted and converts into chunks of packet_size
+        transmit_file = []
+        with open(file_name, "rb") as file:
+            while not file.closed:
+                chunk = file.read(PKTSIZE)
+                if not chunk:
+                    break
+                transmit_file.append(chunk)
         return transmit_file
 
     def transmit(self, file_name, addr):
         # create a udp socket for transmission
+        tx_socket = 
         # divide the file into several parts
         #transmit_file = self.read_file(file_name)
         #packet_num = len(transmit_file)
